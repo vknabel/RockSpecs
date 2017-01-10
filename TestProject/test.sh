acf343e70b69574b4308cd8f1d9f1c3dbf8126d4
Valentin Knabel <dev@vknabel.com>
2017-01-10 11:07:52 +0100

    Commented test.sh

--- TestProject/test.sh
+++ TestProject/test.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# Fail on error
 set -eu -o pipefail
 
+# Create Rockfile
 rock init
 
+# Add each spec to Rockfile
 for specFile in ../Specs/*.yaml; do
   rocket=$(basename $specFile .yaml)
   if [ "$rocket" != "empty" ]
@@ -12,6 +15,7 @@ for specFile in ../Specs/*.yaml; do
   fi
 done
 
+# Use local repository as global spec
 mkdir -p .rock/specs/
 ln -s ../../../ .rock/specs/global
 ROCK_PATH=./.rock rock install
