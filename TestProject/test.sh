d42f1664355f95f6c8896d087430b6d9a310a820
Valentin Knabel <dev@vknabel.com>
2017-03-13 11:02:39 +0100

    Fixes error in test script

--- TestProject/test.sh
+++ TestProject/test.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
+rm -rf Rockfile .rock
+
 # Fail on error
 set -eu -o pipefail
 
 # Create Rockfile
-rock init
+echo "name: TestProject
+url: https://github.com/RockSpecs/TestProject
+version: 0.1.0
+dependencies:
+" > Rockfile
 
 # Add each spec to Rockfile
 for specFile in ../Specs/*.yaml; do
   rocket=$(basename $specFile .yaml)
   if [ "$rocket" != "empty" ]
   then
-    echo "  - $rocket" >> Rockfile
+    echo "- $rocket" >> Rockfile
   fi
 done
 
 # Use local repository as global spec
-mkdir -p .rock/specs/
-ln -s ../../../ .rock/specs/global
-ROCK_PATH=./.rock rock install
+SWIFT_VERSION=$(swiftenv local) ROCK_PATH=./.rock rock install
